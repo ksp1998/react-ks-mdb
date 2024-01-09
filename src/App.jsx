9653086fe853eca1bdbc5700bf9043e98e20645d
kiran suthar <ksuthar2016@gmail.com>
2024-01-09 13:42:51 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center m-8">
+      <h1>KS MDB</h1>
+      <img src="logo-title.svg" />
+    </div>
+  );
+};
+
+export default App;
